c5a465059e443cbe69767680d201688e7abb712c
Nguyễn Quang Đại <dai13829vn@gmail.com>
2025-06-19 09:06:50 +0700

    Add files via upload

---
+++ TOOL/setting.py
@@ -0,0 +1,23 @@
+title = r"""
+$$$$$$$\       $$$$$$$$\  $$$$$$\   $$$$$$\  $$\       
+$$  __$$\      \__$$  __|$$  __$$\ $$  __$$\ $$ |      
+$$ |  $$ |        $$ |   $$ /  $$ |$$ /  $$ |$$ |      
+$$ |  $$ |$$$$$$\ $$ |   $$ |  $$ |$$ |  $$ |$$ |      
+$$ |  $$ |\______|$$ |   $$ |  $$ |$$ |  $$ |$$ |      
+$$ |  $$ |        $$ |   $$ |  $$ |$$ |  $$ |$$ |      
+$$$$$$$  |        $$ |    $$$$$$  | $$$$$$  |$$$$$$$$\ 
+\_______/         \__|    \______/  \______/ \________|
+-------------------------------------------------------"""
+
+def clear_screen():
+    import os
+    if os.name == 'nt':  # Windows
+        os.system('cls')
+    else:  # macOS/Linux
+        os.system('clear')
+def delay(seconds):
+    import time
+    import colorama
+    for i in range(seconds, -1, -1):
+        print(f'{colorama.Fore.CYAN}Vui lòng chờ {i} giây                    ', end="\r")
+        time.sleep(1)
